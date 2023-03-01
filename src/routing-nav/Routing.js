387b2f23ba6f6667503761c2b48710d91c1be123
jtompkins415 <john.tompkins415@gmail.com>
2023-02-28 16:10:03 -0800

    import components

--- src/routing-nav/Routing.js
+++ src/routing-nav/Routing.js
@@ -1,19 +1,24 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import CompanyList from '../companies/CompanyList';
+import CompanyDetails from '../companies/CompanyDetails';
+import JobList from '../jobs/JobList'
+import LoginForm from '../auth/LoginForm'
+import SignupForm from '../auth/SignupForm'
+import Profile from '../profiles/Profile';
+import Homepage from '../homepage/Homepage';
 
 
-
-
-const Routing = () => {
+const Routing = ({login, signup}) => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Homepage />} />
                     <Route path='/companies' element={<CompanyList />}/>
-                    <Route path='/companies/:name' element= {<CompanyDetails />}/>
+                    <Route path='/companies/:handle' element= {<CompanyDetails />}/>
                     <Route path='/jobs' element={<JobList />}/>
-                    <Route path='/login' element={<LoginForm />} />
-                    <Route path='/signup' element={<SignupForm />} />
+                    <Route path='/login' element={<LoginForm login={login} />} />
+                    <Route path='/signup' element={<SignupForm signup={signup} />} />
                     <Route path='/:username' element={<Profile />} />
                 </Routes>
             </BrowserRouter>
