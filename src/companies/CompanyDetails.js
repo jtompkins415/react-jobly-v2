25f518aa02b812b01b4a60131e5a995aaf7062a3
jtompkins415 <john.tompkins415@gmail.com>
2023-02-28 16:08:08 -0800

    start CompanyDetails component

--- src/companies/CompanyDetails.js
+++ src/companies/CompanyDetails.js
@@ -1,5 +1,23 @@
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import JoblyApi from '../api';
+
 
 const CompanyDetails = () => {
+    const {handle} = useParams();
+    const [company, setCompany] = useState(null)
+
+    useEffect(() => {
+        async function getCompany(){
+            let result = await JoblyApi.getCompany(handle);
+            setCompany(result);
+        }
+
+        getCompany()
+    }, [handle])
+
+    
+
     return (
         <div className="CompanyDetails">
             <div className="CompanyDetails-title-wrapper">
