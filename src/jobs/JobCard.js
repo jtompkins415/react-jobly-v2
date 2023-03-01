b4a7b4cf9c720e6094a3446a566edc62d4adf04e
jtompkins415 <john.tompkins415@gmail.com>
2023-03-01 10:42:59 -0800

    create JobCard component

---
+++ src/jobs/JobCard.js
@@ -0,0 +1,30 @@
+import {
+    Card,
+    CardBody,
+    CardText,
+    CardTitle,
+    Button
+} from 'reactstrap';
+import './JobCard.css'
+
+const JobCard = ({title, salary, compName, equity, handle}) => {
+    return (
+        
+            <Card className='job-card'>
+                <CardBody>
+                    <CardTitle  className='job-card_name' tag='h5'>
+                        {title}
+                    </CardTitle>
+                    <CardText className='job-card_info'>
+                        Company: <a href={`/companies/${handle}`}>{compName}</a>
+                        <br/>
+                        Expected Salary: {salary}
+                        <br/>
+                        Expected Equity: {equity}
+                    </CardText>
+                </CardBody>
+            </Card>
+    )
+};
+
+export default JobCard;
