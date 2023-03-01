08debfdf4997c8e430b4e6248081562fd0f135a5
jtompkins415 <john.tompkins415@gmail.com>
2023-02-28 16:08:48 -0800

    use search function to render companies

--- src/companies/CompanyList.js
+++ src/companies/CompanyList.js
@@ -1,11 +1,47 @@
+import {useState, useEffect} from 'react';
+import {CardGroup, Spinner} from 'reactstrap';
+import SearchForm from '../common/SearchForm';
+import JoblyApi from '../api';
+import CompanyCard from './CompanyCard';
 
 const CompanyList = () => {
+    const [companies, setCompanies] = useState(null)
+    
+    
+    useEffect(() => {
+        search();
+     }, [])
+
+     const search = async (name) => {
+        let companies = await JoblyApi.getCompanies(name);
+        setCompanies(companies)
+     }
+
+    
+     console.log(companies)
+
+     if(!companies) return <div><Spinner color='warning'/></div>
+    
+
     return (
         <div className="CompanyList">
             <div className="CompanyList-title-wrapper">
                 <h1> Companies </h1>
             </div>
-
+            <SearchForm searchFor={search} />
+            {companies.length ? (
+                <CardGroup>
+               {companies.map(comp => (
+                <CompanyCard
+                    key={comp.handle}
+                    handle={comp.handle}
+                    name={comp.name}
+                    description={comp.description}
+                    logoUrl={comp.logoUrl} />
+            ))}
+            </CardGroup>) : (
+                <p>Sorry, no results were found</p>
+            )}
         </div>
     )
 };
