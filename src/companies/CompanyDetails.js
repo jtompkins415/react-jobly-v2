0387d464e4bdc1e91ed99a1a682b6bd300111a82
jtompkins415 <john.tompkins415@gmail.com>
2023-03-01 10:42:00 -0800

    destructure from company state, use values for rendering

--- src/companies/CompanyDetails.js
+++ src/companies/CompanyDetails.js
@@ -1,29 +1,45 @@
 import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import JoblyApi from '../api';
+import {Spinner} from 'reactstrap';
 
 
 const CompanyDetails = () => {
     const {handle} = useParams();
     const [company, setCompany] = useState(null)
+    const [jobs, setJobs] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
         async function getCompany(){
             let result = await JoblyApi.getCompany(handle);
             setCompany(result);
+            setIsLoaded(true)
         }
 
         getCompany()
     }, [handle])
 
-    
+    if(!isLoaded) return <Spinner>Loading...</Spinner>
+
+    console.log(company)
+    const {name, description, numEmployees} = company
 
     return (
         <div className="CompanyDetails">
             <div className="CompanyDetails-title-wrapper">
-                <h1> Company Details </h1>
+                <h1> {name} </h1>
             </div>
-
+            <div className="CompanyDetails-body-wrapper">
+                <div>
+                    {description}
+                    <br/>
+                    Number of Employees: {numEmployees}
+                </div>
+            </div>
+            <div className='CompanyDetails-job-wrapper'>
+                <h5>Jobs</h5>
+            </div>
         </div>
     )
 };
