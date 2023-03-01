2dd3595074d4b63e5e74b728888135d07e1054f3
jtompkins415 <john.tompkins415@gmail.com>
2023-02-28 16:06:45 -0800

    remove boilerplate, flush out app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
+import './App.css';
+import Routing from './routing-nav/Routing';
+import Navigation from './routing-nav/Navigation';
+import {decodeToken } from 'react-jwt';
+import UserContext from './auth/UserContext';
+import { useContext } from 'react';
+import JoblyApi from './api';
+import {Spinner} from 'reactstrap';
+import useLocalStorage from './hooks/useLocalStorarge';
+import { BrowserRouter } from 'react-router-dom';
+
+
+export const TOKEN_STORAGE = 'token';
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE)
+  const [infoLoaded, setInfoLoaded] = useState(false);
+
+  /** Effect to get current user.
+   * 
+   * If there is a token present in localStorage,
+   * get username from token,
+   * use JoblyAPI to talk to back and retrieve user obj
+   */
+
+  useEffect(() => {
+    const getCurrUser = async () => {
+      if (token){
+        try{
+          let {username} = decodeToken(token);
+          let currUser = await JoblyApi.getCurrUser(username);
+          setCurrentUser(currUser)
+        } catch(err){
+          console.error("App getUserInfo: problem loading", err);
+          setCurrentUser(null)
+        }
+      }
+      setInfoLoaded(true);
+    }
+    setInfoLoaded(false);
+    getCurrUser()
+  }, [token]);
+
+
+  /** Handles site-wide logout */
+  
+  const logout = () => {
+    setCurrentUser(null);
+    setToken(null);
+  };
+
+  /** Handle site-wide login */
+
+  const login = async (data) => {
+    try { 
+      let result = await JoblyApi.userLogin(data);
+      setToken(result);
+      return {success: true};
+    } catch (err) {
+      console.error('Login Failed', err);
+      return {success: false, err}
+    }
+  }
+
+  /** Handle site-wide registration 
+   * sets token upon sign up
+  */
+
+  const signup = async (data) => {
+    try{
+      let result = await JoblyApi.userSignup(data);
+      setToken(result)
+      return {success: true};
+    }catch(err){
+      console.error('Signup Failed', err);
+      return {success: false, err};
+    } 
+  }
+
+  if(!infoLoaded) return <Spinner>Loading...</Spinner>
+
+
   return (
+    <UserContext.Provider value={{currentUser, setCurrentUser}}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navigation logout={logout} />
+      </BrowserRouter>
+      <Routing login={login} signup={signup}/>
     </div>
+    </UserContext.Provider>
   );
 }
 
