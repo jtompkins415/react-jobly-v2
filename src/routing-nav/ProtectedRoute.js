ae970f6b399caedee2941cac110897d5756e9553
jtompkins415 <john.tompkins415@gmail.com>
2023-03-03 07:58:08 -0800

    edit import of UserContext

--- src/routing-nav/ProtectedRoute.js
+++ src/routing-nav/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import {Route, Routes, Navigate } from 'react-router-dom';
-import UserContext from "../auth/UserContext";
+import {UserContext} from "../auth/UserContext";
 
 const ProtectedRoute = ({ path, component: Component, ...rest}) => {
     const {currentUser} = useContext(UserContext);
